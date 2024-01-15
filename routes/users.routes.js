d43e509d592809621f389be04bf1ed1f94a51ac0
Varvara Korableva <v.korableva1990@yandex.ru>
2024-01-15 12:38:46 +0200

    start registration

---
+++ routes/users.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+//const { auth } = require("../middlewares/utils.js");
+const users_router = express.Router();
+/*const {
+    createUser
+} = require("../controllers/users.controller.js");
+
+
+
+users_router.post('/signup', createUser)
+*/
+
+module.exports = {
+  users_router,
+};
