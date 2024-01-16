6c15969bf4b3f033f35d1ef5eb32f689f6b28f1e
iuriimcqa <iuriimcqa@gmail.com>
2024-01-16 19:35:25 +0200

    tonewbranch

--- public/calculate/calc.js
+++ public/calculate/calc.js
@@ -36,6 +36,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   }})
 
+
+  const form = document.forms[0];
+  
+
+
   function AddFood(title, amount) {
     return fetch(
       `https://api.api-ninjas.com/v1/nutrition?query=${amount} ${title}&X-Api-Key=S1qqU4se/app/0TyiTWsDw==PzMrGtkYUf4ryTn2`
@@ -48,7 +53,10 @@ document.addEventListener("DOMContentLoaded", function () {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
+        const div = document.getElementById('list');
+        const newfood = document.createElement('p');
+        newfood.innerText = `${data[0].name} | ${data[0].serving_size_g}gr| ${data[0].calories}ccal`;
+        div.append(newfood)
         return data;
       })
       .catch((err) => {
@@ -58,6 +66,9 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   AddFood('banana', '100g');
+  AddFood('snickers', '40g');
+  AddFood('fries', '400g');
+  AddFood('melon', '1kg');
    
   
  
